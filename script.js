ef32d50acc4b1d2e7262bb1692b1ae98969bbc71
kevincywang <kevinddtw@gmail.com>
2015-06-12 15:22:04 -0700

    Correcting codes with existing files from temp server

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
-"<li><a href='customers.html'>Data</a></li>" +
-"<li><a href='about.html'>About</a></li>" +
+"<li><a href='hobbies.html'>Hobbies</a></li>" +
+"<li><a href='schedule.html'>Schedule</a></li>" +
+"<li><a href='ultimate_club.html'>Ultimate Layout</a></li>" +
+
 "</ul>";
