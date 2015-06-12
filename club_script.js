62d699481cb367ced70f47e05f305ea9b55b19e8
kevincywang <kevinddtw@gmail.com>
2015-06-12 15:01:26 -0700

    Adding existing html files, first attempt.

---
+++ club_script.js
@@ -0,0 +1,5 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='ultimate_club.html'>Ultimate Layout</a></li>" +
+"<li><a href='club_map.html'>Location</a></li>" +
+"</ul>";
