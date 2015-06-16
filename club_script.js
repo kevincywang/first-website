7f8953e056114a33de64544235c4d42958f19f09
kevincywang <kevinddtw@gmail.com>
2015-06-16 16:49:53 -0700

    Removed Ultimate Layout Branch

--- club_script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='ultimate_club.html'>Ultimate Layout</a></li>" +
-"<li><a href='club_map.html'>Location</a></li>" +
-"</ul>";
