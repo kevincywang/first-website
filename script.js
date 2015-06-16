172d6ad8be7edc29fc43d9a8ad9ab8cbb72b452e
kevincywang <kevinddtw@gmail.com>
2015-06-16 16:53:15 -0700

    Removed Ultimate Layout Page

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='hobbies.html'>Hobbies</a></li>" +
+"<li><a href='schedule.html'>Schedule</a></li>" +
+
+"</ul>";
